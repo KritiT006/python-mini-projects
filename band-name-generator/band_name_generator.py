69fa4895fb235d10975de825ffaa96c4c72e90e5
Kriti <tennerangamkriti@gmail.com>
2026-03-08 03:13:53 +0530

    Refactor band name generator prompts and output

--- band-name-generator/band_name_generator.py
+++ band-name-generator/band_name_generator.py
@@ -1,4 +1,4 @@
-print("Welcome to the Band Name Generator")
-city = input("Which city did you grow up in?")
-pet = input("What is the name of your pet?")
-print("Your band name could be:  “ city  + " " + pet”)
+print("Hello! Welcome to Band Name Generator")
+city=input("What city did u grow up in?\n")
+pet=input("what is the name of your pet?\n")
+print(f"Your Band name is {city}{pet}")
